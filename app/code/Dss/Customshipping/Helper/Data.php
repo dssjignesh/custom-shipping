6a3a6923a93d66fca364d26653ef5ad6b23d9964
kavita <kavitak@digitsoftsol.com>
2024-08-14 18:33:24 +0530

    fix coading standard file

--- app/code/Dss/Customshipping/Helper/Data.php
+++ app/code/Dss/Customshipping/Helper/Data.php
@@ -39,7 +39,6 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
         protected ModuleListInterface $moduleList,
         protected LoggerInterface $logger
     ) {
-        $this->logger = $context->getLogger();
         parent::__construct($context);
     }
 
@@ -54,33 +53,5 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
             self::XML_PATH_ENABLED,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
-    }
-
-    /**
-     * Get Debug Status
-     */
-    public function getDebugStatus()
-    {
-        return $this->scopeConfig->getValue(
-            self::XML_PATH_DEBUG,
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-        );
-    }
-
-    /**
-     * Get debug log status
-     *
-     * @param string $message
-     * @param bool $useSeparator
-     */
-    public function log(string $message, bool $useSeparator = false)
-    {
-        if ($this->getDebugStatus()) {
-            if ($useSeparator) {
-                $this->logger->addDebug(str_repeat('=', 100));
-            }
-
-            $this->logger->addDebug($message);
-        }
     }
 }
--- app/code/Dss/Customshipping/view/frontend/web/js/view/shipping-rates-validation.js
+++ app/code/Dss/Customshipping/view/frontend/web/js/view/shipping-rates-validation.js
@@ -1,6 +1,15 @@
 /**
- * Copyright © 2015 Magento. All rights reserved.
- * See COPYING.txt for license details.
+ * Digit Software Solutions..
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ *
+ * @category   Dss
+ * @package    Dss_Customshipping
+ * @author     Extension Team
+ * @copyright Copyright (c) 2024 Digit Software Solutions. ( https://digitsoftsol.com )
  */
 /*browser:true*/
 /*global define*/
